e3b254106e843979aa294cfababd967adde43769
Kleitomberg <51670381+Kleitomberg@users.noreply.github.com>
2023-04-06 20:29:11 -0300

    toggle theme

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="w-1/2 mx-auto my-20">
+
+<div class="">
+<!---->
+  <header class="my-3 flex justify-end p-2">
+
+
+    <div
+    :class="[
+    'relative',
+    'inline-flex',
+    'w-16',
+    'h-7',
+    'rounded-lg',
+    { 'bg-gray-500': !isActive, 'bg-gray-300': isActive }
+    ]"
+    >
+      <button
+      :class="[
+        'w-7',
+        'h-7',
+        'rounded-full',
+        'transition-transform',
+        'duration-300',
+        'ease-in-out',
+        { 'translate-x-full bg-blue-500': isActive, 'bg-gray-300 left-0': !isActive }
+      ]"
+      @click="toggleButton"
+      ></button>
+    </div>
+
+</header>
+
+  <div :class="['w-1/2 mx-auto my-20']">
+
+
 
     <h1 class="text-3xl font-semibold text-orange-600">WELCOME TO KBLOG</h1>
 
@@ -11,13 +45,16 @@
 
     <!--<button class="bg-orange-300 rounded-md p-2" @click="addPost">Add Postagem</button>-->
   </div>
+</div>
+
 </template>
 
 <script>
 import CardPost from '../components/CardPost.vue';
 import supabase  from '../src/data/supabaseClient';
 
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
+
 
 export default {
     name: "IndexPage",
@@ -26,7 +63,14 @@ export default {
     setup() {
 
         const posts = ref([])
+        const isActive = ref(false)
         const error = ref(null)
+        const theme = ref('light')
+
+        function toggleButton() {
+          isActive.value = !isActive.value
+          console.log(isActive.value)
+        }
 
 
         async function getPosts() {
@@ -45,6 +89,26 @@ export default {
           console.log(data, error)
         }
 
+
+
+        function toggleTheme() {
+          const body = document.querySelector('body')
+          if (theme.value === 'dark') {
+            body.classList.add('dark')
+            body.classList.remove('light')
+
+          } else {
+            body.classList.add('light')
+            body.classList.remove('dark')
+          }
+        }
+
+        watch(isActive, (val) => {
+          theme.value = val ? 'dark' : 'light'
+          toggleTheme()
+
+        })
+
         onMounted(() => {
           getPosts()
         })
@@ -54,7 +118,10 @@ export default {
         return {
           posts,
           error,
-          addPost
+          addPost,
+          toggleButton,
+          isActive,
+          theme
         }
     }
 
@@ -65,6 +132,15 @@ export default {
 
 </script>
 
-<style scoped>
+<style>
+
+.dark {
+  background-color: #1a202c;
+  color: #fff;
+}
+.light {
+  background-color: #fff;
+  color: #000;
+}
 
 </style>
